aae6b42e9c47d7ff6b75034080c4542639177e5c
Radwan Faris <radwanfaris13@gmail.com>
2022-09-27 22:01:33 +0200

    initiated test_projectremaning funcs

--- test_project.py
+++ test_project.py
@@ -1,4 +1,4 @@
-from project import cli, option, music, video
+from project import cli, option, get_tag, music, video
 import pytest
 
 def test_cli():
@@ -8,5 +8,13 @@ def test_cli():
     assert cli("--video") == {'music': False, 'video': True}
 
 
-def test_option():
-    print(option.tags)
+def test_get_tag():
+    dict = {
+        "l_options":[{'itag': '139', 'qu': '48', 'num': 1}, {'itag': '140', 'qu': '128', 'num': 2}],
+        "type_":"audio"
+        }
+    assert get_tag(dict) ==  "140"
+
+
+# def test_option():
+#     print(option.tags)
