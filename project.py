fd086ea76ebef23d427d1e819613ed3ea9879598
Radwan Faris <radwanfaris13@gmail.com>
2022-10-06 11:34:52 +0200

    adjusted some funcs in to be able to test them

--- project.py
+++ project.py
@@ -18,17 +18,19 @@ def main():
         print(video())
 
 
-def option(data, type_="video"):
+def option(data, type_):
     """return a list of the available options and according to the given data,
     take the input from the user acordding to the printed data,
     and consider the input as the itag for the quality
     for either the muisc func or the verdio fun
     - only supprted format utill now is Mb4"""
-
+    
     options = {}
     l_options = []
     num = 1
 
+    if not data:
+        raise ValueError("Couldn't handel the data!")
     # get all the mb4 options and store them in a the option dict
     for i in data:
         # for music search for the itag and the abr value e.g abr=140kbps which like p for vidoes for the quality
@@ -57,13 +59,17 @@ def option(data, type_="video"):
         # we need to recreat another dict because we can't override the values data because they are str\
         # str, int, floot, tuple and so on are immutable
         options = {}
-
+    if not l_options:
+        raise ValueError("Couldn't handel the data!!")
     return {"l_options": l_options, "type_": type_}
+    
 
 
-# this func to render the options to the user so he can choose an option and then return the choosed tag
-# get the list of options and the type of the file and a num equivalent to the itag number, as a dict
-def get_tag(dict):
+""" this func to render the options to the user so he can choose an option and then return the choosed tag
+get the list of options and the type of the file and a num equivalent to the itag number, as a dict
+we have the itag parm to be able to pass it's value from the test file  
+"""
+def get_tag(dict, itag=None):
     l_options = dict["l_options"]
     type_ = dict["type_"]
 
@@ -87,12 +93,13 @@ def get_tag(dict):
     for i in l_options:
         tags.append(i["num"])
         print(f"Choose: {i['num']}, for {i['qu']}{q}")
-
-    try:
-        itag = int(input("Choice: "))
-    except ValueError:
-        print("Invalid Choise")
-        sys.exit()
+    
+    if not itag:
+        try:
+            itag = int(input("Choice: "))
+        except:
+            print("Invalid Choise")
+            sys.exit()
 
     # if input is not in the renderd data then exit with an error message
     if not itag in tags:
@@ -107,26 +114,35 @@ def get_tag(dict):
     return itag
 
 
+
 # download muisc only
-def music(url=None):
+""" we use all of the following args to be able to pass them form the test func in tests file,
+without passing them the func will work normally too """
+
+def music(url=None, download="y", itag=None, data=None):
     # get the data of that file
     try:
-        if url := input("URL: ").strip():
-            pass
-        else:
-            sys.exit("invalid url")
+        if not url:
+            if url := input("URL: ").strip():
+                pass
+            else:
+                sys.exit("invalid url")
 
         print("Please wait ....")
         yt = YouTube(url)
-        data = yt.streams.filter(only_audio=True)
+        
+        if not data:
+            data = yt.streams.filter(only_audio=True)
 
         # option will render the avai options itags so the user can choose one of them and then returns it's itag
-        itag = int(get_tag(option(data, type_="audio")))
+        if not itag:
+            itag = int(get_tag(option(data=data, type_="audio")))
         print("Please wait ....")
 
         # download the music file with the choosen itag and save it in the music folder
         stream = yt.streams.get_by_itag(itag)
-        stream.download("./music/")
+        if download == "y":
+            stream.download("./music/")
 
     except:
         # TODO we nedd to parse the exceptions and hundel them individually
@@ -135,28 +151,34 @@ def music(url=None):
     return "the file has been downloaded sucssfully"
 
 
-# download  video only
-def video(url=None):
+""" download  video only
+# we use all of the following args to be able to pass them form the test func in tests file,
+without passing them the func will work normally too """
+def video(url=None, download="y", itag=None, data=None):
     # get the data of that file
     try:
-        if url := input("URL: ").strip():
-            pass
-        else:
-            sys.exit("Invaild url")
+        if not url:
+            if url := input("URL: ").strip():
+                pass
+            else:
+                sys.exit("Invaild url")
 
         print("Please wait ....")
 
         yt = YouTube(url)
-        data = yt.streams.filter(progressive=True)
+        if not data:
+            data = yt.streams.filter(progressive=True)
 
         # option will render the avai options so the user can choose one of them and then returns it's itag
-        itag = int(get_tag(option(data, type_="video")))
+        if not itag:
+            itag = int(get_tag(option(data=data, type_="video")))
         print("Please wait ....")
 
         # download the video with the choosen itag and save it in the videos folder
 
         stream = yt.streams.get_by_itag(itag)
-        stream.download("./videos/")
+        if download=="y":
+            stream.download("./videos/")
 
     except:
         # TODO we nedd to parse the exceptions and hundel them individually
@@ -168,18 +190,22 @@ def video(url=None):
 def cli(argv=None):
     # we use argv to be able to pass a value from the test file
     if argv:
-        sys.argv[1] = argv
+        try:
+            sys.argv[1] = argv
 
+        except IndexError:
+            raise IndexError("couldn't assign argv to sys.argv[1]")
+    
     """ handel the CLI args get the option [-m | --music] | [-v|--video]
     then return the option"""
 
     parser = argparse.ArgumentParser(
-        description="download a music or a video file from youtube"
+        description="download an audio or video file from youtube"
     )
     # parser.add_argument("-s", help="for test with pytest")
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
-        "-m", "--music", action="store_true", help="download file in a muisc formate"
+        "-m", "--music", action="store_true", help="download file in a audio formate"
     )
     group.add_argument(
         "-v", "--video", action="store_true", help="download file in a video format"
