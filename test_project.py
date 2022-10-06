4d0f17b68061ccd56e73824b3c7d18d98c6d1655
Radwan Faris <radwanfaris13@gmail.com>
2022-10-06 12:05:44 +0200

    add funcs to test each func in project.py

--- test_project.py
+++ test_project.py
@@ -1,34 +1,64 @@
 from project import cli, option, get_tag, music, video
-import pytest
+from pytube import YouTube
+
+
 
 def test_cli():
-    assert cli("-m") == {'music': True, 'video': False}
-    assert cli("--music") == {'music': True, 'video': False}
-    assert cli("-v") == {'music': False, 'video': True}
-    assert cli("--video") == {'music': False, 'video': True}
+    assert cli(argv="-m") == {"music": True, "video": False}
+    assert cli(argv="--music") == {"music": True, "video": False}
+    assert cli(argv="-v") == {"music": False, "video": True}
+    assert cli(argv="--video") == {"music": False, "video": True}
+
+# store the date returned from option func globaly, to use for the get_tag func 
+filtered_data_audio = 0
+filtered_data_video = 0
+
+# store the itags returned from get_tag func globaly so we can use them in muisc and viedo test funcs
+itag_video = 0
+itag_audio = 0
 
 def test_option():
-    ...
-    # TODO
+    yt = YouTube("https://youtu.be/5ceaqtWhdnI") 
+    # store the date returned from the option func to use later with music and video and get_tag funcs
+
+    global filtered_data_audio
+    # for music/ audio data
+    data = yt.streams.filter(only_audio=True)
+    global filtered_data_audio
+    filtered_data_audio = option(data, "audio")
+    assert filtered_data_audio
+
+    # for video data
+    data = yt.streams.filter(progressive=True)
+    global filtered_data_video
+    filtered_data_video =  option(data, "video")
+    assert filtered_data_video
+
 
 def test_get_tag():
-    ...
-    # TODO
+    # choose the first itag , and store the returned itag in a global var to user with music and viedo func
+    # for audio data
+    global filtered_data_audio
+    global itag_audio
+    itag_audio = get_tag(filtered_data_audio, itag=1) 
+    assert itag_audio 
+    
+    # for viedo data
+    global filtered_data_video
+    global itag_video
+    itag_video = get_tag(filtered_data_video, itag=1)
+    assert itag_video
 
+
+# use the same data form itag and option funcs 
 def test_muisc():
-    ...
-    # TODO
+    assert music(
+        url="https://youtu.be/5ceaqtWhdnI", download="n", itag=itag_audio, data=filtered_data_audio
+        ) == "the file has been downloaded sucssfully"
 
+
+# use the same data form itag and option funcs 
 def test_video():
-    ...
-    # TODO
-# def test_get_tag():
-#     dict = {
-#         "l_options":[{'itag': '139', 'qu': '48', 'num': 1}, {'itag': '140', 'qu': '128', 'num': 2}],
-#         "type_":"audio"
-#         }
-#     assert get_tag(dict) ==  "140"
-
-
-# def test_option():
-#     print(option.tags)
+    assert video(
+         url="https://youtu.be/5ceaqtWhdnI", download="n", itag=itag_video, data=filtered_data_video
+    ) == "the file has been downloaded sucssfully"
